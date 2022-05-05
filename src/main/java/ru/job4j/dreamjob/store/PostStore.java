8f18033e89e2f744dfabfbdd71ba2d1b5ff8ee12
Denis Yakovinov <d.yakovinov@mail.ru>
2022-05-05 20:34:48 +0300

    add AtomicInteger id counter to the PostStore

--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -6,16 +6,18 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PostStore {
     private static final PostStore INST = new PostStore();
-
+    private final AtomicInteger idCounter = new AtomicInteger();
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
     private PostStore() {
         posts.put(1, new Post(1, "Junior Job", "java dev", LocalDate.now()));
         posts.put(2, new Post(2, "Middle Job", "java dev", LocalDate.now()));
         posts.put(3, new Post(3, "Senior Job", "java dev", LocalDate.now()));
+        idCounter.addAndGet(posts.size());
     }
 
     public static PostStore instOf() {
@@ -27,7 +29,7 @@ public class PostStore {
     }
 
     public void create(Post post) {
-        post.setId(posts.size() + 1);
+        post.setId(idCounter.incrementAndGet());
         posts.putIfAbsent(post.getId(), post);
     }
 
