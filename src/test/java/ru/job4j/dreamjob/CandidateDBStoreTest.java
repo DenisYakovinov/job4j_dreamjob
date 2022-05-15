abed9a0d9bcb659f7d918585244850496cd53857
Denis Yakovinov <d.yakovinov@mail.ru>
2022-05-15 18:39:11 +0300

    add tests

---
+++ src/test/java/ru/job4j/dreamjob/CandidateDBStoreTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.dreamjob;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.store.CandidateDBStore;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class CandidateDBStoreTest {
+
+    @Test
+    public void whenCreatePost() {
+        CandidateDBStore store = new CandidateDBStore(new Main().loadPool());
+        Candidate candidate = new Candidate(1, "name", "description", LocalDate.now());
+        store.add(candidate);
+        Candidate candidateInDB = store.findById(candidate.getId());
+        assertThat(candidateInDB.getName(), is(candidate.getName()));
+    }
+
+    @Test
+    public void whenUpdateCandidate() {
+        CandidateDBStore store = new CandidateDBStore(new Main().loadPool());
+        Candidate candidate = new Candidate(1, "updatedName", "description", LocalDate.now());
+        store.update(candidate);
+        Candidate candidateInDB = store.findById(candidate.getId());
+        assertThat(candidateInDB.getName(), is(candidate.getName()));
+    }
+
+    @Test
+    public void whenUpdateCandidatePhotoBytes() {
+        CandidateDBStore store = new CandidateDBStore(new Main().loadPool());
+        Candidate candidate = new Candidate(1, "updatedName", "description", LocalDate.now(),
+                new byte[]{0, 12, 33, -127, 127});
+        store.update(candidate);
+        Candidate candidateInDB = store.findById(candidate.getId());
+        assertThat(candidateInDB.getPhoto(), is(candidate.getPhoto()));
+    }
+}
---
+++ src/test/java/ru/job4j/dreamjob/PostDBStoreTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.dreamjob;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.dreamjob.model.City;
+import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.store.PostDBStore;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class PostDBStoreTest {
+
+    @Test
+    public void whenCreatePost() {
+        PostDBStore store = new PostDBStore(new Main().loadPool());
+        Post post = new Post(1, "name", "description", LocalDate.now(), false, new City(1, "default city"));
+        store.add(post);
+        Post postInDb = store.findById(post.getId());
+        assertThat(postInDb.getName(), is(post.getName()));
+    }
+
+    @Test
+    public void whenUpdatePost() {
+        PostDBStore store = new PostDBStore(new Main().loadPool());
+        Post post = new Post(1, "updatedName", "description", LocalDate.now(), false, new City(1, "default city"));
+        store.update(post);
+        Post postInDb = store.findById(post.getId());
+        assertThat(postInDb.getName(), is(post.getName()));
+    }
+
+    @Test
+    public void whenUpdatePostsCity() {
+        PostDBStore store = new PostDBStore(new Main().loadPool());
+        Post post = new Post(1, "Another Job", "smt about", LocalDate.now(), false, new City(2, "another city"));
+        store.update(post);
+        Post postInDb = store.findById(post.getId());
+        assertThat(postInDb.getCity().getId(), is(post.getCity().getId()));
+    }
+}
